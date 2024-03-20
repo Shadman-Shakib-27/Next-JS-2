af5a785d0c692b68ab31bb0fe1fc0d0c4834f503
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2024-03-21 01:47:49 +0600

    Some Part Added.

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "About Page",
+  description: "This is About Page",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>This is About Page.</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
